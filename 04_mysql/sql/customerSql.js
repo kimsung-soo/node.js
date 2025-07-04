60c87196d22f8d240612ecdcbe443c736b59ebb0
unknown <toptensu@naver.com>
2025-07-04 11:05:41 +0900

    env추가

--- 04_mysql/sql/customerSql.js
+++ 04_mysql/sql/customerSql.js
@@ -3,4 +3,5 @@ module.exports = {
   customerInsert: "insert into customers set ?",
   customerUpdate: "update customers set ? where id = ?",
   customerDelete: "delete from customers where id=?",
+  customerSql: "?",
 };
